5ce43956d5141d4cd587906d60e270e38836c280
FilipeSkmc <filipe-santana@outlook.com>
2023-12-07 15:45:03 -0300

    fix

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,58 +1,26 @@
 import { screen } from '@testing-library/react';
 import { vi } from 'vitest';
-
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from './helpers/renderWithRouter';
-import fetchMock from './mocks/fetchMock';
+import { renderWithRouterAndRedux } from './helpers/renderWith';
 
-beforeEach(() => {
-  // espiona a função global.fetch e substitui por uma função mockada
-  // toda vez que a função global.fetch for chamada, ela vai chamar a função mockada
-  vi.spyOn(global, 'fetch').mockImplementation(fetchMock as any);
-});
+import { mockEpisodes } from './mocks/mockEpisodes';
+import { mockPage1 } from './mocks/mockPage1';
 
-describe('Testando a chamada de api', () => {
-  it('Testando se chama o mock de Episodes', async () => {
-    renderWithRouter(<App />);
+describe('Testando a Home', () => {
+  it('Testando se a lista de episodios aparece', async () => {
 
-    const headingElement = await screen.findByText(/Lista de episódios/i);
-    expect(headingElement).toBeInTheDocument();
-
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith('https://rickandmortyapi.com/api/episode');
   });
 
-  it('Testando se chama o mock de Personagens', async () => {
-    renderWithRouter(<App />, { route: '/personagens' });
+  it('Testando se a lista de personagens aparece', async () => {
 
-    const headingElement = await screen.findByText(/personagens/i);
-    expect(headingElement).toBeInTheDocument();
+  });
 
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character/?page=1');
+  it('Testando se a página de Favoritos está limpa', async () => {
 
-    const rickElement = await screen.findByText(/Rick Sanchez/i);
-    expect(rickElement).toBeInTheDocument();
+  });
 
-    const buttonElement = await screen.findByText(/próximo/i);
-    expect(buttonElement).toBeInTheDocument();
+  it('Testando se a página de Favoritos está com os personagens', async () => {
 
-    await userEvent.click(buttonElement);
-
-    const aquaMortyElement = await screen.findByText(/Aqua Morty/i);
-    expect(aquaMortyElement).toBeInTheDocument();
-
-    expect(global.fetch).toHaveBeenCalledTimes(2);
-    expect(global.fetch).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character/?page=2');
-
-    const buttonBeforeLoading = await screen.findByText(/próximo/i);
-    await userEvent.click(buttonBeforeLoading);
-
-    const bigHeadElement = await screen.findByText(/Big Boobed Waitress/i);
-    expect(bigHeadElement).toBeInTheDocument();
-
-    expect(global.fetch).toHaveBeenCalledTimes(3);
-    expect(global.fetch).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character/?page=3');
   });
 });
